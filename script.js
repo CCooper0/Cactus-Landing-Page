6353ae8e538214fa3fc1b2bb6d931e19accd7977
C Cooper <cfcooper@uci.edu>
2019-06-10 00:12:26 -0700

    Fix navbar for mobile/small screens

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   //this toggle changes class list every time menu is clicked: it adds class="change" to things that have class="menu"
 
-  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("navbar").classList.toggle("change");
 }
